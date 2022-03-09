5efa71e6b3ea43a45fafecfe462a012a4d1e186a
Trisha Chakraborty <trisha123chak@gmail.com>
2022-03-09 09:28:50 -0600

    Calculating mean delay

--- ML-Policy-PoW/src/com/policy/main/SolvePuzzle.java
+++ ML-Policy-PoW/src/com/policy/main/SolvePuzzle.java
@@ -2,6 +2,7 @@ package com.policy.main;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Date;
 
 import com.policy.model.ClientSide;
@@ -14,19 +15,29 @@ public class SolvePuzzle {
 		ClientSide clientobj = new ClientSide();
 		clientobj.setR_string("110110");
 		clientobj.setReqID("190.12.19.10");
-		clientobj.setDifficulty(20);
+		clientobj.setDifficulty(4);
 				
 		
 		Puzzle puzzle= new Puzzle(clientobj.getReqID(),clientobj.getR_string(),clientobj.getDifficulty());
+	    int[] timeArray = new int[10];
 	    
-		long startTime = System.currentTimeMillis();
-		long elapsedTime = 0L;		
+	    for(int i=0;i<10;i++) {
+	    	long startTime = System.currentTimeMillis();
+			long elapsedTime = 0L;		
 
-		puzzle.POW(clientobj.getDifficulty());
+			puzzle.POW(clientobj.getDifficulty());
 
-		elapsedTime = (new Date()).getTime() - startTime;
-
-		System.out.println(elapsedTime);
+			elapsedTime = (new Date()).getTime() - startTime;
+			
+			timeArray[i] = (int) elapsedTime;
+			
+			//System.out.println(elapsedTime);
+	    }
+	    
+	    Arrays.sort(timeArray);
+	    System.out.println(timeArray[5]);
+	    
+		
 	}
 
 }
